b4c7571fcc3572ca50a747d76ceb298a3bf2a41d
robpap96 <robertopapagni1996@outlook.it>
2023-02-17 15:55:29 +0100

    aggiunto service seeder

--- database/seeders/ApartmentSeeder.php
+++ database/seeders/ApartmentSeeder.php
@@ -14,6 +14,6 @@ class ApartmentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        
     }
 }
--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Service;
+use Illuminate\Support\Facades\Schema;
 
 class ServiceSeeder extends Seeder
 {
@@ -12,8 +14,33 @@ class ServiceSeeder extends Seeder
      *
      * @return void
      */
+
+
     public function run()
     {
-        //
+        Schema::disableForeignKeyConstraints();
+        Service::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $services = [
+            'Wifi',
+            'Vasca',
+            'Vasca idromassaggio',
+            'Spa zonarelax',
+            'Cancellazione gratuita', 
+            'Animali ammessi',
+            'Parcheggio gratuito', 
+            'Area intrattenimento bimbi',
+            'TV',
+            'Stanza climatizzata',
+            'Servizio navetta',
+            'Zona fumatori',
+        ];
+
+        foreach ($services as $service) {
+            $new_service = new Service();
+            $new_service->name = $service;
+            $new_service->save();
+        }
     }
 }
