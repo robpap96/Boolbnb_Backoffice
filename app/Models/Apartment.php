3fd7f0d5eaca6339ddcdc4a9a31155cdceec110a
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-02-17 14:00:38 +0100

    Aggiunta migration dei services con la tabella pivot e mappati i modelli

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -19,4 +19,8 @@ class Apartment extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function services() {
+        return $this->belongsToMany(Service::class);
+    }
 }
