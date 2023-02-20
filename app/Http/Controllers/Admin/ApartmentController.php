d9f09076d26c89ad93e7aff1e858478a79417162
mariaximenahpg <federico.businessrico@gmail.com>
2023-02-20 14:53:38 +0100

    show.blade singolo appartamento

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -79,7 +79,7 @@ class ApartmentController extends Controller
      */
     public function show(Apartment $apartment)
     {
-        return view('admin.apartments.show');
+        return view('admin.apartments.show', compact('apartment'));
     }
 
     /**
