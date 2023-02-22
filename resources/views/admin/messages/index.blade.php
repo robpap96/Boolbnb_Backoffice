d868422251e0f22cce07b5ec08acaa033157382d
robpap96 <robertopapagni1996@outlook.it>
2023-02-22 11:01:39 +0100

    aggiunte card messages

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -1,18 +1,22 @@
 @extends('layouts.main-dashboard')
 
 @section('content')
-    <div class="messages-container">
+    <div class="messages-container m-3">
         <div class="message_list_title text-center mb-3">
             <h1>I tuoi messaggi</h1>
         </div>
+        
         @foreach ($messages as $message)
-            <div class="message-card m-2 d-flex">
-                <div class="message-image-box">
-                    <div class="message-type {{$message->email}}"><h4>{{$message->email}}</h4></div>
-                    <p>{{ $message->content }}</p>
+        <div class="col-12 mb-5">
+            <div class="card" >
+                <div class="card-body">
+                  <h5 class="card-title">{{$message->email}}</h5>
+                  <p class="card-text">{{ $message->content }}</p>
+                  <a class="btn btn-success" href=" {{route('admin.apartments.show', $message->apartment_id)}}">Vedi l'appartamento</a>
+    
                 </div>
             </div>
-            <a class="btn btn-success" href=" {{route('admin.apartments.show', $message->apartment_id)}}">Vedi l'appartamento</a>
+        </div>
             @endforeach
     </div>
     @endsection
