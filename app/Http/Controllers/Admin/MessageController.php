f9a1efc2d77a8b0e4189d168e25bb1d1f593daf3
robpap96 <robertopapagni1996@outlook.it>
2023-02-23 12:10:30 +0100

    fixati messaggi

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Models\Message;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
     public function index()
     {
         // Get all apartments owned by a logged in user
-        $messages = Message::all();
+        $messages = Message::with('apartment')->get();
+        $user_id = Auth::user()->id;
 
-        return view('admin.messages.index', compact('messages'));
+        return view('admin.messages.index', compact('messages', 'user_id'));
     }
 
 }
--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -5,18 +5,19 @@
         <div class="message_list_title text-center mb-3">
             <h1>I tuoi messaggi</h1>
         </div>
-        
+
         @foreach ($messages as $message)
-        <div class="col-12 mb-5">
-            <div class="card" >
-                <div class="card-body">
-                  <h5 class="card-title">{{$message->email}}</h5>
-                  <p class="card-text">{{ $message->content }}</p>
-                  <a class="btn btn-success" href=" {{route('admin.apartments.show', $message->apartment_id)}}">Vedi l'appartamento</a>
-    
+            @if ( $message['apartment']['user_id'] === $user_id )
+            <div class="col-12 mb-5">
+                <div class="card" >
+                    <div class="card-body">
+                        <h5 class="card-title">{{$message->email}}</h5>
+                        <p class="card-text">{{ $message->content }}</p>
+                        <a class="btn btn-success" href="{{route('admin.apartments.show', $message->apartment )}}">Vedi l'appartamento</a>
+                    </div>
                 </div>
             </div>
-        </div>
-            @endforeach
+            @endif
+        @endforeach
     </div>
     @endsection
