ad4ec6d42ff5b3f78e1dd749d01c38333b0a10a5
robpap96 <robertopapagni1996@outlook.it>
2023-02-23 15:43:46 +0100

    modificato messaggi

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -10,11 +10,16 @@
             @if ( $message['apartment']['user_id'] === $user_id )
             <div class="col-12 mb-5">
                 <div class="card" >
-                    <div class="card-body p-3">
+                    <div class="card-body mask-custom p-3">
                         <h5 class="card-title mb-2">{{$message->email}}</h5>
+                        <p class="text small mb-0"><i class="far fa-clock"></i> Ricevuto il {{$message->created_at}}</p>
                         <p class="card-text p-4">{{ $message->content }}</p>
-                        <a class="btn btn-success" href="{{route('admin.apartments.show', $message->apartment )}}">Vedi l'appartamento</a>
-                        a scopo
+                        <div class="div">
+                            <a class="btn btn-success" href="{{route('admin.apartments.show', $message->apartment )}}">Vedi l'appartamento <i class="fa-sharp fa-solid fa-eye"></i></a>
+                            {{-- a scopo illustrativo --}}
+                            <a class="btn btn-primary" href="#">Rispondi <i class="fa-solid fa-reply"></i></a>
+                        </div>
+
 
                     </div>
                 </div>
