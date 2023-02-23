f2d713bdfa2f230c987e5d5d9afe5671dd5c7029
robpap96 <robertopapagni1996@outlook.it>
2023-02-23 13:02:47 +0100

    aggiunto un po di colore ai messaggi

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.main-dashboard')
 
 @section('content')
-    <div class="messages-container m-3">
-        <div class="message_list_title text-center mb-3">
-            <h1>I tuoi messaggi</h1>
+    <div id="admin-messages-index" class="messages-container m-3">
+        <div class="message_list_title mb-3">
+            <h2>Messaggi Ricevuti:</h2>
         </div>
 
         @foreach ($messages as $message)
             @if ( $message['apartment']['user_id'] === $user_id )
             <div class="col-12 mb-5">
                 <div class="card" >
-                    <div class="card-body">
-                        <h5 class="card-title">{{$message->email}}</h5>
-                        <p class="card-text">{{ $message->content }}</p>
+                    <div class="card-body p-3">
+                        <h5 class="card-title mb-2">{{$message->email}}</h5>
+                        <p class="card-text p-4">{{ $message->content }}</p>
                         <a class="btn btn-success" href="{{route('admin.apartments.show', $message->apartment )}}">Vedi l'appartamento</a>
+                        a scopo
+
                     </div>
                 </div>
             </div>
