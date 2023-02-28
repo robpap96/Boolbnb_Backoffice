143659b9268645d4ef8be48319ed17c0ce340f4f
PaoloNapoli97 <paolonapoli@live.it>
2023-02-28 12:50:56 +0100

    Cambiato stile bottoni della Inxed message

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -9,19 +9,20 @@
 
             @foreach ($messages as $message)       
                 <div class="col-12 mb-5">
-                    <div class="apartment-details services mt-2 mb-4">
+                    <div class="message-details services mt-2 mb-4">
                         <h6 class="field p-1 px-3 text-center text-white d-flex justify-content-between">
                             <span>{{$message->email}}</span>
                             <span>{{$message->created_at}}</span>
                         </h6>
-                        <div class="bg-light details-body h-100 py-3 px-3 d-flex flex-wrap justify-content-start align-items-center">
+                        <div class="bg-light details-body h-100 py-3 px-3 d-flex flex-wrap aling-item-start justify-content-center flex-column">
                             {{$message->content}}
+                            <div class="div pt-3 pb-1 d-flex">
+                                <a class="my-btn btn-show-apartment" href="{{route('admin.apartments.show', $message->apartment )}}">Vedi l'appartamento <i class="fa-sharp fa-solid fa-eye"></i></a>
+                                {{-- a scopo illustrativo --}}
+                                <a class="my-btn btn-reply" href="mailto:{{ $message->email }}">Rispondi <i class="fa-solid fa-reply"></i></a>
+                            </div>
                         </div>
-                        <div class="div py-3">
-                            <a class="btn btn-success" href="{{route('admin.apartments.show', $message->apartment )}}">Vedi l'appartamento <i class="fa-sharp fa-solid fa-eye"></i></a>
-                            {{-- a scopo illustrativo --}}
-                            <a class="btn btn-primary" href="mailto:{{ $message->email }}">Rispondi <i class="fa-solid fa-reply"></i></a>
-                        </div>
+
                     </div>
                 </div>
             @endforeach
