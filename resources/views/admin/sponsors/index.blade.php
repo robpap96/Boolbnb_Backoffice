16687e6e8fa1262cf19676f1a5b335130fda9f14
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-03-02 14:59:07 +0100

    Modificato style della pagina delle sponsorizzazioni

--- resources/views/admin/sponsors/index.blade.php
+++ resources/views/admin/sponsors/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="sponsor-container">
         <div class="sponsor_list_title text-center mb-3">
-            <h1 class="m-text-cursive">Segli la promozione più adatta a te!</h1>
+            <h1 class="m-text-cursive">Segli la promozione più adatta a te !</h1>
         </div>
 
         <div class="sponsors-card-container d-flex">
@@ -21,7 +21,7 @@
 
                         <div class="sponsor-price">
                             <a href="{{ route('admin.sponsors.show', $sponsor->slug) }}">
-                                <button class="btn w-100" type="submit">Aquista {{ $sponsor->price }} €</button>
+                                <button class="btn w-100" type="submit">Aquista per {{ $sponsor->price }} €</button>
                             </a>
                         </div>
                     </div>
@@ -29,18 +29,19 @@
             @endforeach
         </div>
 
-        <p class="">
-            <h3>Come funziona ?</h3>
-            Un appartamento sponsorizzato ha le seguenti particolarità:
-            <ul>
+        <h2 class="m-text-cursive text-center mt-2 mb-0">Come funziona ?</h2>
+        <div class="row align-items-center m-2 mt-0 sponsors-how">
+            <div class="col-9">
+                <h5>Un appartamento sponsorizzato ha le seguenti particolarità:</h5>
+
                 <li>Appare in Homepage nella sezione “Appartamenti in Evidenza”</li>
-                <li>
-                    Nella pagina di ricerca, viene posizionato sempre prima di un appartamento non
-                    sponsorizzato che soddisfa le stesse caratteristiche di ricerca
-                </li>
-            </ul>
-            Terminato il periodo di sponsorizzazione, l’appartamento tornerà ad essere
-            visualizzato normalmente, senza alcuna particolarità.
-        </p>
+                <li>Nella pagina di ricerca, viene posizionato sempre prima di un appartamento non sponsorizzato 
+                che soddisfa le stesse caratteristiche di ricerca</li>
+            </div>
+            <div class="col-3 text-center">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-cETReGrSEnsS4Sef4AY1eAzkVECciLGPjA&usqp=CAU" alt="">
+            </div>
+            <small class="mt-3 text-center">Terminato il periodo di sponsorizzazione, l’appartamento tornerà ad essere visualizzato normalmente, senza alcuna priorità.</small>
+        </div>
     </div>
 @endsection
