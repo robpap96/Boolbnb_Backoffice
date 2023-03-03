cc7fc377eaba0b0ec24e593f75de0ff035b92700
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-03-03 14:46:52 +0100

    gestito pagamento sponsors

--- resources/views/admin/sponsors/show.blade.php
+++ resources/views/admin/sponsors/show.blade.php
@@ -1,80 +1,213 @@
 @extends('layouts.main-dashboard')
 
 @section('content')
-    @if (session('message'))
-        <div class="alert alert-success m-2">
-            {{ session('message') }}
-        </div>
-    @endif
-
     <div class="sponsor-container">
-        <div class="sponsor-card mb-3 d-flex">
-            <div class="sponsor-image-box">
-                <img class="sponsor-image {{$sponsor->name}}" src="{{ Vite::asset('resources/img/IMG-20230220-WA0002_origin.png')}}" alt="">
+        {{-- Sponsor card with apartments select --}}
+        <div class="sponsor-card">
+            <div class="sponsor-image-box text-center">
+                <img class="sponsor-image {{$sponsor->name}}" src="{{ Vite::asset('resources/img/IMG-20230220-WA0002_origin.png')}}" alt="" width="150px" height="150px">
                 <div class="sponsor-type {{$sponsor->name}}"><h4>{{$sponsor->name}}</h4></div>
             </div>
 
-            <div class="sponsor-info d-flex justify-content-between w-100">
-                <div class="sponsor-dettagli d-flex flex-column ">
-                    <h3>Pacchetto {{ $sponsor->name }}</h3>
-                    <p>Il pacchetto {{ $sponsor->name }} ti garantisce fino a {{ $sponsor->duration }} Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
-                    <small class="mt-auto">Durata promozione: {{ $sponsor->duration}} ore</small>
-                </div>
-            </div>
-        </div>
-
-        <form action="{{ route('admin.sponsors.buy', $sponsor->name) }}">
-            <select name="apartment_sponsored" id="" class="form-select">
-                <option value="" selected hidden>Lista appartamenti</option>
-                @if ( $apartments->isEmpty() )
-                    <option value="no_apartments" disabled>Pare che tu non abbia ancora creato un appartamento. Creane subito uno!</option>
-                @else 
-                    @foreach ($apartments as $apartment)
-                        @if ($apartment->is_visible) 
-                            <option value="{{$apartment->id}}">{{ $apartment->title }} - {{ $apartment->full_address }}</option>
-                        @else 
-                            <option class="text-muted select_disabled" value="{{$apartment->id}}" disabled>{{ $apartment->title }} - {{ $apartment->full_address }}</option>
-                        @endif
-                    @endforeach
-                @endif
-            </select>
-            @error('apartment_sponsored')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
+            <form id="sponsors-buy-form" action="{{ route('admin.sponsors.buy', $sponsor->name) }}">
+                <select name="apartment_sponsored" id="" class="form-select">
+                    <option value="" selected hidden>Scegliere un appartamento da sponsorizzare</option>
+                    @if ( $apartments->isEmpty() )
+                        <option value="no_apartments" disabled>Pare che tu non abbia ancora creato un appartamento. Creane subito uno!</option>
+                    @else 
+                        @foreach ($apartments as $apartment)
+                            @if ($apartment->is_visible) 
+                                <option value="{{$apartment->id}}">{{ $apartment->title }} - {{ $apartment->full_address }}</option>
+                            @else 
+                                <option class="text-muted select_disabled" value="{{$apartment->id}}" disabled>{{ $apartment->title }} - {{ $apartment->full_address }}</option>
+                            @endif
+                        @endforeach
+                    @endif
+                </select>
+                @error('apartment_sponsored')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </form>
 
             <div class="my-3 alert alert-warning">
                 <small><strong>Nota bene:</strong> Gli appartamenti non visibili non potranno essere sponsorizzati.</small>
             </div>
-            
-            <div class="payments w-75 m-auto">
-                <div id="dropin-container"></div>
-                <button type="button" id="submit-button" class="btn btn-success">Aquista {{ $sponsor->price }} €</button>
-                <a href="{{ route('admin.sponsors.index') }}" class="btn btn-secondary">Indietro</a>
+        </div>
+
+        {{-- Make payment container --}}
+        <div class="payments"> 
+            <div class="form-container m-0">
+                <form id="my-sample-form" class="scale-down">
+                    <div class="cardinfo-card-number">
+                        <label class="cardinfo-label" for="card-number">Numero identificativo carta</label>
+                        <div class='input-wrapper' id="card-number"></div>
+                        <div id="card-image"></div>
+                    </div>
+                
+                    <div class="cardinfo-wrapper">
+                        <div class="cardinfo-exp-date">
+                        <label class="cardinfo-label" for="expiration-date">Valida fino al </label>
+                        <div class='input-wrapper' id="expiration-date"></div>
+                        </div>
+                
+                        <div class="cardinfo-cvv">
+                        <label class="cardinfo-label" for="cvv">CVV</label>
+                        <div class='input-wrapper' id="cvv"></div>
+                        </div>
+                    </div>
+                </form>
+                
+                <input id="button-pay" type="submit" value="Acquista per {{$sponsor->price}} €" />
             </div>
-        </form>
+        </div>
+
+        {{-- Payment confirm --}}
+        @if (session('message'))
+            <div id="toast-payment" class="toast align-items-center show w-auto" role="alert" aria-live="assertive" aria-atomic="true">
+                <div class="d-flex">
+                <div class="toast-body d-flex align-items-center">
+                    <i class="fa-solid fa-check fa-fw fa-lg me-1"></i>
+                    <span>
+                        {{ session('message') }}
+                    </span>
+                </div>
+                <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+            </div>
+        @endif
     </div>
 
-    <script src="https://js.braintreegateway.com/web/dropin/1.35.0/js/dropin.js"></script>
+    {{-- Logic card payments --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
+    <script src="https://js.braintreegateway.com/web/3.91.0/js/hosted-fields.min.js"></script>
+    <script src="https://js.braintreegateway.com/web/3.91.0/js/client.min.js"></script>
     <script>
-        var button = document.querySelector('#submit-button');
+        var form = document.querySelector('#my-sample-form');
+        var submit = document.querySelector('input[type="submit"]');
 
-        braintree.dropin.create({
-        authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b',
-        selector: '#dropin-container'
-        }, function (err, instance) {
-            button.addEventListener('click', function () {
-                instance.requestPaymentMethod(function (err, payload) {
-                    if ( payload !== undefined ) {
-                        // Pagamento andato a buon fine
-                        let paymenSubmitButton = document.getElementById('submit-button');
-                        paymenSubmitButton.type = 'submit';
-                        paymenSubmitButton.click();
-                    } else {
-                        // Pagamento non andato a buon fine
-                        console.log('compila i campi !')
-                    }
+        braintree.client.create({
+        authorization: 'sandbox_g42y39zw_348pk9cgf3bgyw2b'
+        }, function (err, clientInstance) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        // Create input fields and add text styles  
+        braintree.hostedFields.create({
+            client: clientInstance,
+            styles: {
+            'input': {
+                'color': '#282c37',
+                'font-size': '16px',
+                'transition': 'color 0.1s',
+                'line-height': '3'
+            },
+            // Style the text of an invalid input
+            'input.invalid': {
+                'color': '#E53A40'
+            },
+            // placeholder styles need to be individually adjusted
+            '::-webkit-input-placeholder': {
+                'color': 'rgba(0,0,0,0.6)'
+            },
+            ':-moz-placeholder': {
+                'color': 'rgba(0,0,0,0.6)'
+            },
+            '::-moz-placeholder': {
+                'color': 'rgba(0,0,0,0.6)'
+            },
+            ':-ms-input-placeholder': {
+                'color': 'rgba(0,0,0,0.6)'
+            },
+            // prevent IE 11 and Edge from
+            // displaying the clear button
+            // over the card brand icon
+            'input::-ms-clear': {
+                opacity: '0'
+            }
+            },
+            // Add information for individual fields
+            fields: {
+            number: {
+                selector: '#card-number',
+                placeholder: '**** **** **** ****'
+            },
+            cvv: {
+                selector: '#cvv',
+                placeholder: '***'
+            },
+            expirationDate: {
+                selector: '#expiration-date',
+                placeholder: 'MM / YYYY'
+            }
+            }
+        }, function (err, hostedFieldsInstance) {
+            if (err) {
+            console.error(err);
+            return;
+            }
+
+            hostedFieldsInstance.on('validityChange', function (event) {
+            // Check if all fields are valid, then show submit button
+            var formValid = Object.keys(event.fields).every(function (key) {
+                return event.fields[key].isValid;
+            });
+
+            if (formValid) {
+                $('#button-pay').addClass('show-button');
+            } else {
+                $('#button-pay').removeClass('show-button');
+            }
+            });
+
+            hostedFieldsInstance.on('empty', function (event) {
+            $('header').removeClass('header-slide');
+            $('#card-image').removeClass();
+            $(form).removeClass();
+            });
+
+            hostedFieldsInstance.on('cardTypeChange', function (event) {
+            // Change card bg depending on card type
+            if (event.cards.length === 1) {
+                $(form).removeClass().addClass(event.cards[0].type);
+                $('#card-image').removeClass().addClass(event.cards[0].type);
+                $('header').addClass('header-slide');
+                
+                // Change the CVV length for AmericanExpress cards
+                if (event.cards[0].code.size === 4) {
+                hostedFieldsInstance.setAttribute({
+                    field: 'cvv',
+                    attribute: 'placeholder',
+                    value: '1234'
                 });
-            })
+                } 
+            } else {
+                hostedFieldsInstance.setAttribute({
+                field: 'cvv',
+                attribute: 'placeholder',
+                value: '123'
+                });
+            }
+            });
+
+            submit.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            hostedFieldsInstance.tokenize(function (err, payload) {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+
+                // This is where you would submit payload.nonce to your server
+                // alert('Submit your nonce to your server here!');
+                let sponsorsBuyForm = document.getElementById('sponsors-buy-form');
+
+                sponsorsBuyForm.submit();
+            });
+            }, false);
+        });
         });
     </script>
 @endsection
