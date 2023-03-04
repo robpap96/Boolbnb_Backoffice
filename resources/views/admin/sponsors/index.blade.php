cfc42bdf3245e8dd63e3c3d0413257538f131482
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-03-04 01:03:04 +0100

    responsive sponsors

--- resources/views/admin/sponsors/index.blade.php
+++ resources/views/admin/sponsors/index.blade.php
@@ -8,13 +8,13 @@
 
         <div class="sponsors-card-container d-flex mt-2">
             @foreach ($sponsorships as $sponsor)
-                <div class="sponsor-card m-2">
+                <div class="sponsor-card m-2 d-flex flex-column">
                     <div class="sponsor-image-box d-flex flex-column align-items-center">
                         <img class="sponsor-image {{$sponsor->name}}" src="{{ Vite::asset('resources/img/IMG-20230220-WA0002_origin.png')}}" alt="">
                         <div class="sponsor-type {{$sponsor->name}}"><h4>{{$sponsor->name}}</h4></div>
                     </div>
 
-                    <div class="sponsor-info">
+                    <div class="sponsor-info mt-auto">
                         <div class="sponsor-dettagli text-center">
                             <p>Il pacchetto {{ $sponsor->name }} ti garantisce <span class="text-decoration-underline">{{ $sponsor->duration }} ore di sponsorizzazione.</span></p>
                         </div>
@@ -31,14 +31,14 @@
 
         <h2 class="m-text-cursive text-center mt-4 mb-0">Come funziona ?</h2>
         <div class="row align-items-center m-2 mt-0 sponsors-how">
-            <div class="col-9">
+            <div class="col-md-9">
                 <h5>Sponsorizzando il tuo appartamento otterrai i seguenti benefici:</h5>
 
                 <li>Apparirà in Homepage nella sezione “Appartamenti in Evidenza”</li>
                 <li>Nelle pagine di ricerca, verrà sempre posizionato prima di un appartamento non sponsorizzato 
                 che soddisfa le stesse caratteristiche di ricerca</li>
             </div>
-            <div class="col-3 text-center">
+            <div class="col-md-3 text-center">
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-cETReGrSEnsS4Sef4AY1eAzkVECciLGPjA&usqp=CAU" alt="">
             </div>
             
